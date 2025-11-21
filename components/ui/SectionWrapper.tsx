1ecc3982f0929e4d9d50f2369839ca1344f74dc3
judetheval-coder <judetheval@gmail.com>
2025-11-21 12:12:53 -0600

    Remove unused layout import

---
+++ components/ui/SectionWrapper.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import { layout } from '../../lib/design'
+
+interface Props { id?: string; className?: string; children: React.ReactNode; }
+
+export const SectionWrapper: React.FC<Props> = ({ id, className = '', children }) => (
+  <section id={id} className={`py-24 ${className}`} style={{ scrollMarginTop: '90px' }}>
+    {children}
+  </section>
+)
+
+export default SectionWrapper
+
